c88ae87996cb4ffbf7c006cfc36ee74ab71bb881
Henry <yanghanyugg@gmail.com>
2016-09-13 01:17:12 -0400

    add 2 solutions
    290. Word Pattern
    6. ZigZag Conversion

---
+++ 290. Word Pattern.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        istringstream in(str);
+        string s;
+        vector<string> vs;
+        while(in >> s) // split str
+            vs.push_back(s);
+        if (pattern.size() != vs.size())
+            return false;
+        map<string, char> s2c;
+        map<char, string> c2s;
+        for (int i = 0; i < vs.size(); ++i) {
+            if (s2c[vs[i]] == 0 && c2s[pattern[i]] == "") { // bijection
+                s2c[vs[i]] = pattern[i]; 
+                c2s[pattern[i]] = vs[i]; 
+            } else if (s2c[vs[i]] != pattern[i]) // detect
+                return false;
+        }
+        return true;
+    }
+};
---
+++ 6. ZigZag Conversion.cpp
@@ -0,0 +1,34 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string result = "";
+        if (numRows == 1)
+			return s;
+        int step1,step2, pos;
+        int len = s.size();
+        for (int i = 0; i < numRows; ++i){
+            // generally 2 steps to get next character
+            step1 = 2 * numRows - 2 * (i + 1); 
+            step2 = 2 * i;
+            pos = i;
+            if (pos < len)
+                result += s[pos]; // add first column
+            while(1) { // keep iterating the 2 steps to get characters
+                pos += step1;
+                if (pos >= len) // detect overflow
+                    break;
+				if (step1) // last row only has step 2
+					result += s[pos];
+                pos += step2;
+                if (pos >= len)
+                    break;
+				if(step2) // first row only has step 1
+					result += s[pos];
+            }
+        }
+        return result;
+    }
+};
