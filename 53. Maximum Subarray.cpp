4ebe61d6ba30819c549d482e74a50bfc74004366
Henry Yang <yanghanyugg@gmail.com>
2016-10-15 23:27:11 -0400

    update comment
    dp thinking

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -3,6 +3,10 @@
 // Space: O(1)
 
 // dp
+// 典型的DP题：
+// 1. 状态dp[i]：以A[i]为最后一个数的所有max subarray的和。
+// 2. 通项公式：dp[i] = dp[i-1]<=0 ? dp[i] : dp[i-1]+A[i]
+// 3. 由于dp[i]仅取决于dp[i-1]，所以可以仅用一个变量来保存前一个状态，而节省内存。
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
